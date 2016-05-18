e0c04458a6e56a27231b417f9fadf21bd49c9881
craig <craig@notebook>
2016-05-18 00:55:22 -0400

    initial commit

---
+++ ca.footeware.web/src/test/java/ca/footeware/web/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package ca.footeware.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import ca.footeware.web.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
