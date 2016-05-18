99a716dbcf2e2d65b08fd50dda9aff4b6479c41d
craig <craig@notebook>
2016-05-18 01:26:03 -0400

    add copyright

--- ca.footeware.web/src/main/resources/static/js/jquery.fancybox.js
+++ ca.footeware.web/src/main/resources/static/js/jquery.fancybox.js
@@ -1,14 +1,6 @@
-/*!
- * fancyBox - jQuery Plugin
- * version: 2.1.4 (Thu, 10 Jan 2013)
- * @requires jQuery v1.6 or later
- *
- * Examples at http://fancyapps.com/fancybox/
- * License: www.fancyapps.com/fancybox/#license
- *
- * Copyright 2012 Janis Skarnelis - janis@fancyapps.com
- *
- */
+/*******************************************************************************
+ * Copyright (c) 2016 Footeware.ca
+ *******************************************************************************/
 
 (function (window, document, $, undefined) {
 	"use strict";
