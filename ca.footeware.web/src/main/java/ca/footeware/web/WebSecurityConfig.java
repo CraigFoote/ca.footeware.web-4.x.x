527b9455fd3dbb44e9efaa3dc45265b99ef0ccbc
craig <craig@notebook>
2017-09-22 18:45:13 -0400

    updated spring IDE

--- ca.footeware.web/src/main/java/ca/footeware/web/WebSecurityConfig.java
+++ ca.footeware.web/src/main/java/ca/footeware/web/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeRequests()
-				.antMatchers("/", "/home", "/gear", "/webcam", "/jokes/**", "/styles/**", "/js/**", "/images/**",
+				.antMatchers("/", "/home", "/gear", "/webcam", "/jokes/**", "/addjoke", "/deletejoke/**", "/styles/**", "/js/**", "/images/**",
 						"/fonts/**")
 				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
 				.logout().permitAll();
